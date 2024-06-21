84a76e1da506e8d21826b3bbc9807772760d0d79
poojithayata <64078180+poojithayata@users.noreply.github.com>
2024-06-21 15:52:46 +0000

    Modifying the insertion with string arrays.

--- Lab_03/pyata_Lab03.c
+++ Lab_03/pyata_Lab03.c
@@ -7,24 +7,60 @@ execute command : ./a
 
 #include <stdio.h>
 #include <stdlib.h>
+# include <string.h>
 
-void displayArray(float *arr, int n){
-    printf("[");
-    for (int i = 0; i < n-1; i++)
-    {
-        printf("%.1f, ", arr[i]);
+char ** Input_Array(int *no){
+
+    // This statement read the number of strings in an array
+    printf("Enter the number of strings in an Array : ");
+    scanf("%d", no);
+
+    // This statement states the dynamic memory allocation for the strings of differen sizes
+    char **stringArray = (char**)malloc((*no) * sizeof(char*));
+    // Handling errors if error is there then program terminates
+    if (stringArray == NULL){
+        printf("Error!....");
+        exit(1);
     }
-    printf("%.1f]\n", arr[n-1]);
+
+    // Declaring a temporary variable
+    char temp[1024];
+
+    for(int i = 0; i < *no; i++){
+        printf("String[%d] : ", i);
+        scanf("%s", temp);
+
+        // Each string memory alloction of the array
+        stringArray[i] = (char*)malloc((strlen(temp)+ 1)* sizeof(char));
+        // Handling errors if error is there then program terminates
+        if(stringArray[i] == NULL){
+            printf("Error!....");
+            exit(1);
+        }
+        // Copying the buffer to the actual array - stringArray
+        strcpy(stringArray[i], temp);
+    }
+
+    return stringArray;
+}
+
+void display_Array(char **string_Array, int num){
+    printf("[");
+    for (int i = 0; i < num-1; i++)
+    {
+        printf("%s, ", string_Array[i]);
+    }
+    printf("%s]\n", string_Array[num-1]);
     
 }
 
-void sortArray(float *arr, int n){
+void insertion_Sort(char **arr, int n){
     printf("Applying Sorting....\n");
-    int key, j;
+    // int key, j;
     for(int i =1; i<n; i++){
-        key = arr[i];
-        j = i-1;
-        while (j >= 0 && arr[j]>key){
+        char *key = arr[i];
+        int j = i-1;
+        while (j >= 0 && strcmp(arr[j],key)>0){
             arr[j+1] = arr[j];     
             j -= 1;
         }
@@ -35,24 +71,23 @@ void sortArray(float *arr, int n){
 int main(int argc, char** argv){
 
     int N;
-    printf("Enter the number of array elements : ");
-    scanf("%d", &N);
+    char **strArray = Input_Array(&N);
 
-    // Dynamic Mmemory Allocation
-    float *arr = (float*)malloc(N*sizeof(float)) ;
-    for(int i =0; i< N; i++){
-        printf("Array[%d] = ", i);
-        scanf("%f", &arr[i]);
-    }
 
-    printf("Array Before Sorting : ");
-    displayArray(arr, N);
+    printf("String Array Before Sorting : ");
+    display_Array(strArray, N);
 
     // Applying insertion sort
-    sortArray(arr, N);
+    insertion_Sort(strArray, N);
 
-    printf("Array After Sorting : ");
-    displayArray(arr, N);
+    printf("String Array After Sorting : ");
+    display_Array(strArray, N);
+
+    // To free the memory of the strArray
+    for(int i =0; i<N; i++){
+        free(strArray[i]);
+    }
+    free(strArray);
 
     return 0;
 }
